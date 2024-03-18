48711b55e36dc719276be454989d8f0046f57bb4
Thaotic-good <fu3martina@gmail.com>
2024-03-18 09:00:15 +0100

    tailwind integration successful

--- src/TechSelector.jsx
+++ src/TechSelector.jsx
@@ -48,9 +48,44 @@ function TechSelector() {
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Button
             </button>
+            <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+                Buy now
+            </button>
             <div className="bg-blue-500 text-red p-4">
                 This is an example using Tailwind CSS classes.
             </div>
+            <form className="space-y-6" action="#" method="POST">
+                <div>
+                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email
+                        address</label>
+                    <div className="mt-2">
+                        <input id="email" name="email" type="email" autoComplete="email" required
+                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="flex items-center justify-between">
+                        <label htmlFor="password"
+                               className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                        <div className="text-sm">
+                            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot
+                                password?</a>
+                        </div>
+                    </div>
+                    <div className="mt-2">
+                        <input id="password" name="password" type="password" autoComplete="current-password" required
+                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit"
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
+                        in
+                    </button>
+                </div>
+            </form>
         </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@
 
 module.exports = {
   content: [
-      './src/**/*.{js, jsx}',
-      './public/index.html'
+      "./src/*.{js, jsx}",
+      "./public/index.html"
   ],
+    purge: ['./src/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {},
   },
